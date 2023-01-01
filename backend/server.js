c77a178e80d75feb3c39972cdaec69a199ea0c32
enas48 <enasm4829@gmail.com>
2023-01-01 17:37:44 +0200

    add login register

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const userRouter = require('./routes/userRoutes');
 app.get('/', (req, res)=>{
     res.send('hi');
 })
+// Curb Cores Error by adding a header here
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
